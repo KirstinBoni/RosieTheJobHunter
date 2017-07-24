3d6943a418d3c41bee16555fbeae6735adc8f27a
Marianna Sternefeld <marianna@sternefeld.com>
2017-07-24 16:29:26 -0700

    Added Bootstrap theme

--- RosieTheJobHunter/App_Start/BundleConfig.cs
+++ RosieTheJobHunter/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace RosieTheJobHunter
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/yeti.bootstrap.css",
                       "~/Content/site.css"));
         }
     }

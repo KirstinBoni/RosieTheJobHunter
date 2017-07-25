c61136c37abc55e47ee07a4a34c1f917a874184b
KirstinBoni <boni.14@osu.edu>
2017-07-25 15:58:33 -0700

    added file

--- RosieTheJobHunter/Controllers/HomeController.cs
+++ RosieTheJobHunter/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using RosieTheJobHunter.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,16 +12,42 @@ namespace RosieTheJobHunter.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
+            await GetJobs();
+            HomeIndexModel model = new HomeIndexModel()
+            {
+
+            };
             return View();
         }
 
-        public ActionResult About()
+        public async Task GetJobs()
         {
-            ViewBag.Message = "Your application description page.";
+            using (HttpClient client = new HttpClient())
+            {
+                StringBuilder uri =  new StringBuilder("https://jobs.github.com/positions.json?");
+                var parameters = new Dictionary<string, string> { { "description", "software+engineering"  },
+                                                                        {"location", "" },
+                                                                        {"full_time", "true"  },
+                                                                      };
 
-            return View();
+                KeyValuePair<string, string> last = parameters.Last();
+                foreach (KeyValuePair<string, string> param in parameters)
+                {
+                    uri.Append(param.Key + "=" + param.Value);
+                    if (!param.Equals(last))
+                    {
+                        uri.Append("&");
+                    }
+                }
+
+                HttpResponseMessage response = await client.GetAsync(uri.ToString());
+
+                string responseContent = await response.Content.ReadAsStringAsync();
+                
+                
+            }
         }
 
         public ActionResult Contact()

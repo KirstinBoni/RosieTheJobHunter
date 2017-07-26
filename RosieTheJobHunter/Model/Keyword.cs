59b693407ef6380098af4548b6ffcc67c3959a79
KirstinBoni <boni.14@osu.edu>
2017-07-25 22:43:15 -0700

    added files

---
+++ RosieTheJobHunter/Model/Keyword.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RosieTheJobHunter.Model
+{
+    public class Keyword
+    {
+        public string language = "en";
+        public string id { get; set; }
+        public string text { get; set; }
+    }
+}
